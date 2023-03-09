0ccfa65435528c31786638518fa8de10ece90795
Alexander <alexandernedrud@gmail.com>
2023-03-09 03:19:26 +0100

    Changed Link to NavLink in button.js

--- src/components/button.js
+++ src/components/button.js
@@ -1,12 +1,12 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 export function Button() {
     return(
         <nav className="category-nav">
-            <Link className="category-nav-button" to="/">React</Link>
-            <Link className="category-nav-button" to="/css">CSS</Link>
-            <Link className="category-nav-button" to="/javascript">Javascript</Link>
-            <Link className="category-nav-button" to="/headless-cms">Headless CMS</Link>
-            <Link className="category-nav-button" to="/html">HTML</Link>
+            <NavLink className="category-nav-button" to="/">React</NavLink>
+            <NavLink className="category-nav-button" to="/css">CSS</NavLink>
+            <NavLink className="category-nav-button" to="/javascript">Javascript</NavLink>
+            <NavLink className="category-nav-button" to="/headless-cms">Headless CMS</NavLink>
+            <NavLink className="category-nav-button" to="/html">HTML</NavLink>
         </nav>
     )
 }
