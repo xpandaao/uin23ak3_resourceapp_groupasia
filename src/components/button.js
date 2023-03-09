db9e366dc3f1c421017bd75f56abc92647181f03
Alexander <alexandernedrud@gmail.com>
2023-03-09 03:57:26 +0100

    Changes to make it more responsive

--- src/components/button.js
+++ src/components/button.js
@@ -2,11 +2,13 @@ import { NavLink } from "react-router-dom";
 export function Button() {
     return(
         <nav className="category-nav">
-            <NavLink className="category-nav-button" to="/">React</NavLink>
-            <NavLink className="category-nav-button" to="/css">CSS</NavLink>
-            <NavLink className="category-nav-button" to="/javascript">Javascript</NavLink>
-            <NavLink className="category-nav-button" to="/headless-cms">Headless CMS</NavLink>
-            <NavLink className="category-nav-button" to="/html">HTML</NavLink>
+            <ul className="category-nav-list">
+                <li className="category-nav-list-item"><NavLink className="category-nav-button" to="/">React</NavLink></li>
+                <li className="category-nav-list-item"><NavLink className="category-nav-button" to="/css">CSS</NavLink></li>
+                <li className="category-nav-list-item"><NavLink className="category-nav-button" to="/javascript">Javascript</NavLink></li>
+                <li className="category-nav-list-item"><NavLink className="category-nav-button" to="/headless-cms">Headless CMS</NavLink></li>
+                <li className="category-nav-list-item"><NavLink className="category-nav-button" to="/html">HTML</NavLink></li>
+            </ul>
         </nav>
     )
 }
